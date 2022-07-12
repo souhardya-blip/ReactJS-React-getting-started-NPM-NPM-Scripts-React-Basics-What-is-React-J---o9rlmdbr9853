a155f9248c7c21792869ab471a45f14b0360068f
meetsouhardya <meetsouhardya@gmail.com>
2022-07-13 00:12:50 +0530

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,48 @@
-import React, { Component, useState, useEffect } from "react";
+import React, {Component, useState, useEffect} from "react";
 import '../styles/App.css';
 
 const App = () => {
-  // write your code here 
+	// write your code here
+	const [time, setTime] = useState(0)
 
-  return (
-    <div className="wrapper">
-      <div id="whole-center">
-        <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
-        </h1>
-      </div>
-      <div id="current-time">{/* remaining time */}</div>
-    </div>
-  )
+
+	useEffect(() => {
+		const timer =
+			time > 0
+			&& setInterval(() => setTime(prevState => prevState - 1), 1000)
+		return () => clearInterval(timer)
+	}, [time])
+
+	if (time < 0) setTime(0)
+
+	const onKeyDown = (event) => {
+		if (event.key !== 'Enter') return
+		const value = event.target.value
+		for (const el of value) {
+			if(isNaN(Number(el))) {
+				setTime(0)
+				event.target.value = '0'
+				return;
+			}
+		}
+		const result = parseInt(value)
+		if (!isNaN(result)) {
+			setTime(result)
+		} else {
+			setTime(0)
+			event.target.value = '0'
+		}
+	}
+	return (
+		<div className="wrapper">
+			<div id="whole-center">
+				<h1>
+					Reverse countdown for<input id="timeCount" onKeyDown={onKeyDown}/> sec.
+				</h1>
+			</div>
+			<div id="current-time">{time}</div>
+		</div>
+	)
 }
 
 
